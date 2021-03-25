dbbbce5ead365f54614a49ecc9aeeb406fe20582
estebankra <estebankrauwezuk@gmail.com>
2021-03-25 17:24:59 -0300

    Solved problem with social items on small screens

--- src/components/AboutMeSection.jsx
+++ src/components/AboutMeSection.jsx
@@ -10,26 +10,26 @@ export default function AboutMeSection() {
       <h1>¡Hi! I'm Esteban Krauwezuk | estebankra</h1>
 
         <div id="social">
-          <ul>
-          <SocialMediaItem 
-            href='mailto:estebankrauwezuk@gmail.com'
-            icon={<FiMail />} 
-          />
-          <SocialMediaItem 
-            href='https://www.linkedin.com/in/estebankra/'
-            icon={<FiLinkedin />}
-            targetBlank
-          />
-          <SocialMediaItem
-            href='https://github.com/estebankra'
-            icon={<FiGithub />}
-            targetBlank
-          />
-          <SocialMediaItem 
-            href='https://codepen.io/estebankra/'
-            icon={<FiCodepen />}
-            targetBlank
-          />
+          <ul className="list-social-items">
+            <SocialMediaItem 
+              href='mailto:estebankrauwezuk@gmail.com'
+              icon={<FiMail />} 
+            />
+            <SocialMediaItem 
+              href='https://www.linkedin.com/in/estebankra/'
+              icon={<FiLinkedin />}
+              targetBlank
+            />
+            <SocialMediaItem
+              href='https://github.com/estebankra'
+              icon={<FiGithub />}
+              targetBlank
+            />
+            <SocialMediaItem 
+              href='https://codepen.io/estebankra/'
+              icon={<FiCodepen />}
+              targetBlank
+            />
           </ul>
         </div>
       </div>
